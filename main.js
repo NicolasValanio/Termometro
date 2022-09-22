dca5efd5dd014c665341c0bfcd7ea0987832e96b
Nicolas Valenzuela <serviciosvalenzuela1@gmail.com>
2022-09-22 14:05:33 -0500

    Final del trabajo

---
+++ main.js
@@ -0,0 +1,10 @@
+function convertir() {
+    let grados =document.getElementById('farent').value;
+    let resultado= ((grados - 32)*5)/9;
+    resultado=parseInt(resultado);
+    let celcius = document.getElementById('label').innerHTML = resultado.toFixed(2);
+    let termometro= document.getElementById('termo').style.backgroundColor = "red";
+    termometro = document.getElementById('termo').style.height = resultado+"%";
+}
+
+  
